4cb75b0ac49c8d6fcce2b1327f7422a0e7c383f6
JoeBakalor <jmbakalor@gmail.com>
2021-07-12 21:01:12 -0500

    remove main

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,19 +1,19 @@
-#include "TeknikioDevice.h"
+// #include "TeknikioDevice.h"
 
-TeknikioDevice device;
+// TeknikioDevice device;
 
-int main()
-{
-    device.init();
+// int main()
+// {
+//     device.init();
 
-    while(1)
-    {
-        device.io.P29.setDigitalValue(1);
-        device.sleep(200);
+//     while(1)
+//     {
+//         device.io.P29.setDigitalValue(1);
+//         device.sleep(200);
 
-        // device.io.P29.setDigitalValue(0);
-        // device.sleep(200);
-    }
-}
+//         // device.io.P29.setDigitalValue(0);
+//         // device.sleep(200);
+//     }
+// }
 
-//python uf2convert.py NRF52840_DK.hex -c -f 0xADA52840 -o bluebird_test.uf2
+// //python uf2convert.py NRF52840_DK.hex -c -f 0xADA52840 -o bluebird_test.uf2
