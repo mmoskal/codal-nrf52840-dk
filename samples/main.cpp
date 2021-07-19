350b1d2a3f9bbafcc9bb9f5af2accfa3f6951a8c
JoeBakalor <jmbakalor@gmail.com>
2021-07-19 12:55:32 -0500

    add bb name

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,19 +1,19 @@
-// #include "TeknikioDevice.h"
+#include "TeknikioDevice.h"
 
-// TeknikioDevice device;
+TeknikioDevice bluebird;
 
-// int main()
-// {
-//     device.init();
+int main()
+{
+    bluebird.init();
 
-//     while(1)
-//     {
-//         device.io.P29.setDigitalValue(1);
-//         device.sleep(200);
+    while(1)
+    {
+        bluebird.io.P29.setDigitalValue(1);
+        bluebird.sleep(200);
 
-//         // device.io.P29.setDigitalValue(0);
-//         // device.sleep(200);
-//     }
-// }
+        // device.io.P29.setDigitalValue(0);
+        // device.sleep(200);
+    }
+}
 
 // //python uf2convert.py NRF52840_DK.hex -c -f 0xADA52840 -o bluebird_test.uf2
